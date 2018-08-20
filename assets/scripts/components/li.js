0dcf1fb83f1a4875809e1124687070ab21b224ef
zaqmjuop <1060007045@qq.com>
2018-08-20 14:53:30 +0800

    添加条形柱

---
+++ assets/scripts/components/li.js
@@ -0,0 +1,61 @@
+import Dom from '../dom';
+
+/** 计数器 */
+let counter = 0;
+const getCounter = () => {
+  counter += 1;
+  return counter;
+};
+
+/** 条形柱组件 */
+
+const param = {
+  query: 'li',
+  url: './assets/templates/li.html',
+  name: 'li',
+  selectors: {
+    getRandom: '*[name=get-random]',
+  },
+  data() {
+    return {
+      value: 0,
+      primaryKey: getCounter(),
+    };
+  },
+  methods: {
+    init() {
+      if (Object.keys(this.present).includes('value')) {
+        this.data.value = this.present.value || 0;
+      }
+      this.data.order = this.data.primaryKey;
+      this.methods.moveToOrder();
+      this.methods.fill();
+    },
+    bindEvents() {
+      /** 接受父组件消息 */
+      this.addEventListener('send', (event) => {
+        if (Number.isSafeInteger(event.detail.value)) {
+          this.data.value = event.detail.value;
+          this.methods.fill();
+        }
+        if (Number.isSafeInteger(event.detail.order)) {
+          this.data.order = event.detail.order;
+          this.methods.moveToOrder();
+        }
+      });
+      /** 接受消息改变位置 */
+    },
+    fill() {
+      Dom.of(this.template).css('height', `${this.data.value * 3}px`);
+      Dom.of(this.template).text(this.data.value);
+    },
+    moveToOrder() {
+      Dom.of(this.template).css('left', `${(this.data.order - 1) * 40}px`);
+    },
+  },
+  created() {
+    this.methods.init();
+    this.methods.bindEvents();
+  },
+};
+export default param;
--- assets/scripts/components/welcome.js
+++ assets/scripts/components/welcome.js
@@ -1,5 +1,13 @@
+import Dom from '../dom';
+import li from './li';
 
-
+/** 获取一个范围在[0,max)区间随机数
+ * @param {number} max - 一个数字表示取值的上限
+ */
+const getRandom = (max = 20) => {
+  if (!Number.isSafeInteger(max) || max < 0) { throw new TypeError(`取值上限不能是${max}`); }
+  return Math.floor(Math.random() * max);
+};
 /** 应用主体组件 */
 
 const param = {
@@ -7,13 +15,97 @@ const param = {
   url: './assets/templates/welcome.html',
   name: 'welcome',
   selectors: {
+    getRandom: '*[name=get-random]',
+    test: '*[name=test]',
+    ul: 'ul',
   },
   data() {
-    return {};
+    return {
+      array: [],
+      items: [],
+    };
   },
   methods: {
+    /** 添加20个子组件li */
+    init() {
+      let promise = Promise.resolve(1);
+      for (let index = 0; index < 20; index += 1) {
+        promise = promise.then(() => this.appendChild(li, this.elements.ul, -1))
+          .then(item => this.data.items.push(item));
+      }
+      return promise;
+    },
+    /** 获取20个随机数 */
+    getRandom(number = 20) {
+      if (!Number.isSafeInteger(number)) { throw new TypeError(`参数number不能是${number}`); }
+      const ary = new Array(number);
+      for (let index = 0; index < ary.length; index += 1) {
+        ary[index] = Math.floor(100 * Math.random());
+      }
+      this.data.array = ary;
+    },
+    /** 按照20个随机数改变子组件li高度 */
+    sendArray() {
+      this.data.items.forEach((item, index) => {
+        item.dispatchEvent('send', { value: this.data.array[index] });
+      });
+    },
+    bindEvents() {
+      // 随机召唤数组
+      Dom.of(this.elements.getRandom).on('click', () => {
+        this.methods.getRandom();
+        this.methods.sendArray();
+      });
+      // 测试方法
+      Dom.of(this.elements.test).on('click', () => {
+        // const item = this.data.items[5];
+        // const random = Math.floor(Math.random() * 20) + 1;
+        // item.dispatchEvent('send', { order: random });
+        const key1 = getRandom(20);
+        let key2;
+        for (let index = 0; index < 1000; index += 1) {
+          key2 = getRandom(20);
+          if (key2 !== key1) { break; }
+        }
+        this.methods.exchange(key1, key2);
+      });
+    },
+    /** 操作Li位置的方法,2个li交换位置
+     * @param {number} index1 - 一个数字，对应this.data.items[index1]
+     * @param {number} index2 - 一个数字，对应this.data.items[index2]
+     */
+    exchange(index1, index2) {
+      if (!Number.isSafeInteger(index1) || index1 < 0 || index1 > 19) {
+        throw new Error(`index1不能是${index1}`);
+      }
+      if (!Number.isSafeInteger(index2) || index2 < 0 || index2 > 19) {
+        throw new Error(`index2不能是${index2}`);
+      }
+      const item1 = this.data.items[index1];
+      const item2 = this.data.items[index2];
+      const order1 = item1.data.order;
+      const order2 = item2.data.order;
+      item1.dispatchEvent('send', { order: order2 });
+      item2.dispatchEvent('send', { order: order1 });
+    },
+    /** 操作li位置的方法，把一个li移动到指定顺位 */
+    /** 操作Li位置的方法,2个li交换位置
+     * @param {number} index1 - 一个数字，对应this.data.items[index1]
+     * @param {number} order - 一个数字，使this.data.items[index1]移动的目标顺位
+     * @todo
+     */
+    makeLiMoveTo(index1, order) {
+      if (!Number.isSafeInteger(index1) || index1 < 0 || index1 > 19) {
+        throw new Error(`index1不能是${index1}`);
+      }
+      if (!Number.isSafeInteger(order) || order < 0 || order > 19) {
+        throw new Error(`order不能是${order}`);
+      }
+    },
   },
   created() {
+    return this.methods.init()
+      .then(() => this.methods.bindEvents());
   },
 };
 export default param;
