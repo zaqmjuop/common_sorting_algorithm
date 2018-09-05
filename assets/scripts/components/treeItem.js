7c4cda1bc3902be091e1bb8c917d2bd34b9ef369
zaqmjuop <1060007045@qq.com>
2018-09-05 23:29:57 +0800

    添加树连接线

--- assets/scripts/components/treeItem.js
+++ assets/scripts/components/treeItem.js
@@ -15,11 +15,14 @@ const param = {
     left: '.left',
     right: '.right',
     value: '.value',
+    bracketTop: '.bracket-top',
+    bracketLeft: '.bracket-left',
+    bracketRight: '.bracket-right',
   },
   methods: {
     init() {
       this.data.value = this.present.value || this.data.value || 0;
-      // Dom.of(this.elements.value).text(this.data.value);
+      Dom.of(this.elements.value).text(this.data.value);
     },
     // 添加左分支
     appendLeft(cpt) {
@@ -27,6 +30,8 @@ const param = {
       return this.appendChild(cpt, this.elements.left, -1)
         .then((item) => {
           this.data.left = item;
+          Dom.of(this.elements.bracketTop).removeClass('hide');
+          Dom.of(this.elements.bracketLeft).removeClass('hide');
           return item;
         });
     },
@@ -36,6 +41,8 @@ const param = {
       return this.appendChild(cpt, this.elements.right, -1)
         .then((item) => {
           this.data.right = item;
+          Dom.of(this.elements.bracketTop).removeClass('hide');
+          Dom.of(this.elements.bracketRight).removeClass('hide');
           return item;
         });
     },
