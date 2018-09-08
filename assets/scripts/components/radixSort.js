6a0b30ca216415338500a12d2e92dc06e7cec2a7
zaqmjuop <1060007045@qq.com>
2018-09-09 02:50:15 +0800

    保存进度

--- assets/scripts/components/radixSort.js
+++ assets/scripts/components/radixSort.js
@@ -1,4 +1,4 @@
-import li from './li';
+
 import Dom from '../dom';
 
 const radixSort = (array) => {
@@ -47,19 +47,28 @@ const param = {
     };
   },
   selectors: {
-    ul: 'ul',
+    canvas: 'canvas',
     getRandom: '*[name=get-random]',
     sort: '*[name=sort]',
   },
   methods: {
     init() {
+      this.data.ctx = this.elements.canvas.getContext('2d');
       let promise = Promise.resolve();
-      for (let index = 0; index < 20; index += 1) {
-        const liParam = Object.assign({ present: { order: index + 1 } }, li);
-        promise = promise.then(() => this.appendChild(liParam, this.elements.ul, -1))
-          .then(item => this.data.items.push(item));
-      }
       return promise;
+    },
+    fillDefault() {
+
+    },
+    run() {
+      this.data.interval = setInterval(() => {
+        this.data.ctx.clearRect(0, 0, this.data.width, this.data.height);
+        this.methods.fillDefault();
+      }, 1000 / this.data.fps);
+      return this.data.interval;
+    },
+    pause() {
+      return clearInterval(this.data.interval);
     },
     /** 获取20个随机数 */
     getRandom(number = 20) {
@@ -76,33 +85,7 @@ const param = {
     /** 按照20个随机数改变子组件li高度 */
     sendArray() {
       this.data.items.forEach((item, index) => {
-        item.dispatchEvent('send', { value: this.data.array[index] });
       });
-    },
-    /** 操作Li位置的方法,2个li交换位置
-   * @param {number} index1 - 一个数字，对应this.data.items[index1]
-   * @param {number} index2 - 一个数字，对应this.data.items[index2]
-   */
-    exchange(index1, index2) {
-      if (!Number.isSafeInteger(index1) || index1 < 0 || index1 > 19) {
-        throw new Error(`index1不能是${index1}`);
-      }
-      if (!Number.isSafeInteger(index2) || index2 < 0 || index2 > 19) {
-        throw new Error(`index2不能是${index2}`);
-      }
-      if (index1 !== index2) {
-        this.data.exchangePromise = this.data.exchangePromise
-          .then(() => {
-            const item1 = this.data.items[index1];
-            const item2 = this.data.items[index2];
-            const order1 = item1.data.order;
-            const order2 = item2.data.order;
-            console.log('替换', item1.data.value, item2.data.value);
-            item1.dispatchEvent('send', { order: order2 });
-            item2.dispatchEvent('send', { order: order1 });
-          });
-      }
-      return this.data.exchangePromise;
     },
     bindEvents() {
       // 随机召唤数组
