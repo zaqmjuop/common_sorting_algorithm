ffe8c82ccb8dc3261ca3115243db69adb6107211
zaqmjuop <1060007045@qq.com>
2018-09-17 04:45:01 +0800

    校对快排动画

--- assets/scripts/components/quickSort.js
+++ assets/scripts/components/quickSort.js
@@ -1,49 +1,7 @@
 import li from './li';
 import Dom from '../dom';
 import utils from '../utils';
-
-function quickSort(array, startIndex = 0, endIndex = array.length - 1) {
-  const isValidArgs = array instanceof Array
-    && Number.isSafeInteger(startIndex)
-    && Number.isSafeInteger(endIndex)
-    && startIndex >= 0
-    && startIndex < endIndex;
-  if (!isValidArgs) { return false; }
-  let rightIndex = endIndex; // 设置右端点
-  let leftIndex = startIndex; // 设置左端点
-  const reference = array[leftIndex]; // 取左端点值为参照物
-  while (leftIndex < rightIndex) { // 左右端点不重合时循环排序
-    for (; leftIndex < rightIndex; rightIndex -= 1) { // 从右端点开始收紧
-      if (array[rightIndex] < reference) {
-        // 如果右端点比参照物小，就赋值到左端点的位置上，由于左端点已经被赋值为目标值，所以左端点就收紧一位。此时右端点是个多余值
-        array.splice(leftIndex, 1, array[rightIndex]); // 把左端点赋值为右端点值
-        // 此时右端点为多余值，参照物不在数组中
-        leftIndex += 1; // 由于左端点已经被赋值，于是将左端点收紧一位
-        break;
-      }
-      // 如果从右侧遍历没有找到比参照物小的值，右端点会收紧到与左端点重合
-    }
-    // 如果已经从右侧找到比参照物小的值而使左右端点未重合，那么开始从左端点开始找比参照物大的值
-    for (; leftIndex < rightIndex; leftIndex += 1) { // 从左端点开始收紧
-      if (array[leftIndex] > reference) {
-        // 如果从左侧找到比参照物大的值，把左端点值赋值到右端点，那么左端点就变成了多余值位置
-        array.splice(rightIndex, 1, array[leftIndex]);
-        rightIndex -= 1; // 由于右端点已经被赋值，于是将左端点收紧一位,然后进入下一次循环
-        break;
-      }
-      // 如果从左侧没有找到比参照物大的值，左端点会收紧到与右端点重合
-    }
-  }
-  // 到这里左右端点重合，且位置是多余值的位置
-  array.splice(leftIndex, 1, reference); // 把参照物赋值回多余值位置
-  if (leftIndex - 1 > 0) {
-    quickSort(array, 0, leftIndex - 1); // 递归左端点的左侧部分
-  }
-  if (endIndex > leftIndex + 1) {
-    quickSort(array, leftIndex + 1, endIndex); // 递归左端点的右侧部分
-  }
-  return array;
-}
+import { getRandomArray } from '../helper';
 
 const param = {
   name: 'quickSort',
@@ -60,61 +18,56 @@ const param = {
     ul: 'ul',
     getRandom: '*[name=get-random]',
     sort: '*[name=sort]',
+    speed: '*[name=speed]',
   },
   methods: {
     init() {
+      // 20个<li>
       let promise = Promise.resolve();
       for (let index = 0; index < 20; index += 1) {
         const liParam = Object.assign({ present: { order: index + 1 } }, li);
-        promise = promise.then(() => this.appendChild(liParam, this.elements.ul, -1))
+        promise = promise
+          .then(() => this.appendChild(liParam, this.elements.ul, -1))
           .then(item => this.data.items.push(item));
       }
       return promise;
     },
     /** 获取20个随机数 */
-    getRandom(number = 20) {
-      if (!Number.isSafeInteger(number)) { throw new TypeError(`参数number不能是${number}`); }
-      const ary = new Array(number);
-      for (let index = 0; index < ary.length; index += 1) {
-        ary[index] = Math.trunc(100 * Math.random()) + 1;
-      }
-      this.data.array = ary;
-      this.data.bubbleSortedTimes = 0;
-      this.data.isBubbleSortDone = 0;
-      this.data.isFinished = false;
-    },
-    /** 按照20个随机数改变子组件li高度 */
-    sendArray() {
+    getRandom() {
+      if (this.data.isRunning) { return false; }
+      this.data.array = getRandomArray();
+      // 改变Li高度
       this.data.items.forEach((item, index) => {
         item.dispatchEvent('send', { value: this.data.array[index] });
       });
+      return this.data.array;
     },
     /** 操作Li位置的方法,2个li交换位置
-   * @param {number} index1 - 一个数字，对应this.data.items[index1]
-   * @param {number} index2 - 一个数字，对应this.data.items[index2]
-   */
-    exchange(index1, index2) {
-      if (!Number.isSafeInteger(index1) || index1 < 0 || index1 > 19) {
+     * @param {number} index1 - 一个数字，对应this.data.items[index1]
+     * @param {number} index2 - 一个数字，对应this.data.items[index2]
+     */
+    exchange(index1, index2, mesc = 200) {
+      if (!Number.isSafeInteger(index1) || index1 < 0 || index1 > this.data.items.length - 1) {
         throw new Error(`index1不能是${index1}`);
       }
-      if (!Number.isSafeInteger(index2) || index2 < 0 || index2 > 19) {
+      if (!Number.isSafeInteger(index2) || index2 < 0 || index2 > this.data.items.length - 1) {
         throw new Error(`index2不能是${index2}`);
       }
-      if (index1 !== index2) {
-        this.data.exchangePromise = this.data.exchangePromise
-          .then(() => {
-            const item1 = this.data.items[index1];
-            const item2 = this.data.items[index2];
-            const order1 = item1.data.order;
-            const order2 = item2.data.order;
-            console.log('替换', item1.data.value, item2.data.value);
-            item1.dispatchEvent('send', { order: order2 });
-            item2.dispatchEvent('send', { order: order1 });
-            this.data.items.splice(index1, 1, item2);
-            this.data.items.splice(index2, 1, item1);
-          });
-      }
-      return this.data.exchangePromise;
+      if (index1 === index2) { return false; }
+      const promise = new Promise((resolve) => {
+        const item1 = this.data.items[index1];
+        const item2 = this.data.items[index2];
+        const order1 = item1.data.order;
+        const order2 = item2.data.order;
+        this.data.items.splice(index1, 1, item2);
+        this.data.items.splice(index2, 1, item1);
+        console.log(`替换 [${index1}]${item1.data.value} <-> [${index2}]${item2.data.value}`);
+        item1.dispatchEvent('send', { order: order2 });
+        item2.dispatchEvent('send', { order: order1 });
+        this.data.exchangeTimes += 1;
+        resolve(utils.wait(mesc));
+      });
+      return promise;
     },
     bindEvents() {
       // 随机召唤数组
@@ -126,37 +79,57 @@ const param = {
         return this.methods.sendArray();
       });
       Dom.of(this.elements.sort).on('click', () => {
-        // 分组 排序 替换原值
+        if (this.data.isRunning) {
+          return console.warn('正在运行中,你可以刷新页面重新开始');
+        }
+        if (this.data.isSorted || this.data.array.every(item => !item || item <= 0)) {
+          this.methods.getRandom();
+          this.data.items.forEach((item) => {
+            item.dispatchEvent('send', { backColor: '' });
+          });
+        }
+        // 排序前
+        this.data.isRunning = true;
+        this.data.speed = 1000 - Number(this.elements.speed.value) * 100;
+        this.data.exchangeTimes = 0;
+        this.data.isSorted = false;
+        this.data.startIndex = 0;
+        this.data.endIndex = this.data.items.length - 1;
         console.log('点击了排序');
-        this.methods.quickSortRecursion()
-          .then(() => console.log(this.data.items.map(item => item.componentId)));
-        console.log(this.data.array);
+        // 排序
+        let promise = Promise.resolve();
+        promise = promise
+          .then(() => this.methods.quickSortRecursion());
+        // 排序后
+        promise = promise
+          .then(() => {
+            this.data.isRunning = false;
+            this.data.isSorted = true;
+            this.data.array = this.data.items.map(item => item.data.value);
+            console.log(`done。 交换次数${this.data.exchangeTimes}`, this.data.array);
+          });
+        return promise;
       });
     },
     quickSortOnce(startIndex = 0, endIndex = this.data.items.length - 1) {
       // 分组
-      /** @todo */
-      let leftIndex = startIndex; // 变化后左端点
-      let rightIndex = endIndex; // 变化后右端点
-      const reference = this.data.items[leftIndex]; // 取左端点值为参照物
+      let leftIndex = startIndex; // 左端点
+      let rightIndex = endIndex; // 右端点
+      const pivot = this.data.items[leftIndex]; // 取左端点值为基准
       // 显示范围
-      for (let index = startIndex; index <= endIndex; index += 1) {
-        const item = this.data.items[index];
-        if (index === leftIndex) {
-          Dom.of(item.template).addClass('yellow');
-        } else {
-          item.methods.select();
-        }
+      Dom.of(this.data.items[leftIndex].template).addClass('yellow');
+      for (let index = leftIndex + 1; index <= rightIndex; index += 1) {
+        this.data.items[index].methods.select();
       }
       // 从右侧收紧 找到第一个较小值停止
       for (; leftIndex < rightIndex; rightIndex -= 1) {
-        if (this.data.items[rightIndex].data.value < reference.data.value) {
-          break; // 没有换值 从哪里开始 rightIndex会在哪 从end到left
+        if (this.data.items[rightIndex].data.value < pivot.data.value) {
+          break;
         }
       }
       // 从左侧收紧 找到第一个较大值停止
       for (; leftIndex < rightIndex; leftIndex += 1) {
-        if (this.data.items[leftIndex].data.value > reference.data.value) {
+        if (this.data.items[leftIndex].data.value > pivot.data.value) {
           break;
         }
       }
@@ -166,51 +139,46 @@ const param = {
         const item = this.data.items[index];
         promise = promise
           .then(() => {
-            // 高亮一秒
-            item.methods.highLight(222);
-            return utils.wait(222);
-          }).then(() => {
-            let async;
-            if (index === rightIndex && rightIndex !== startIndex) {
-              // 怎么收紧rightIndex
-              async = this.methods.exchange(startIndex, index)
-                .then(() => {
-                  item.methods.unselect();
-                  utils.wait(222);
-                });
-            } else {
-              item.methods.unselect();
-              async = utils.wait(222);
+            // 高亮一下
+            item.methods.highLight(this.data.speed);
+            return utils.wait(this.data.speed);
+          })
+          .then(() => {
+            if (index !== rightIndex || rightIndex === startIndex) {
+              return false;
             }
-            return async.then(() => console.log(this.data.items.map(item1 => item1.data.order)));
+            return this.methods.exchange(startIndex, index);
+          })
+          .then(() => {
+            // 取消高亮
+            item.methods.unselect();
+            return utils.wait(this.data.speed);
           });
       }
-
       // 左向右动画
       for (let index = startIndex + 1; index <= leftIndex; index += 1) {
         const item = this.data.items[index];
         promise = promise
           .then(() => {
-            // 高亮一秒
-            if (index === leftIndex && item.data.value <= reference.data.value) { return false; }
-            item.methods.highLight(222);
-            return utils.wait(222);
-          }).then(() => {
-            let async;
-            if (index === leftIndex && leftIndex !== rightIndex) {
-              async = this.methods.exchange(rightIndex, index)
-                .then(() => {
-                  item.methods.unselect();
-                  utils.wait(222);
-                });
-            } else {
-              item.methods.unselect();
-              async = utils.wait(222);
+            if (index === leftIndex && item.data.value <= pivot.data.value) {
+              return false;
             }
-            return async;
+            // 高亮一下
+            item.methods.highLight(this.data.speed);
+            return utils.wait(this.data.speed);
+          }).then(() => {
+            if (index !== leftIndex || leftIndex === rightIndex) {
+              return false;
+            }
+            return this.methods.exchange(rightIndex, index);
+          })
+          .then(() => {
+            // 取消高亮
+            item.methods.unselect();
+            return utils.wait(this.data.speed);
           });
       }
-      // 从左向右收紧，右侧端点是focus 只能是先算出结果在写动画
+      // 交换之后
       promise = promise
         .then(() => {
           this.data.startIndex = leftIndex;
@@ -219,70 +187,51 @@ const param = {
       return promise;
     },
     quickSortLoop() {
-      let promise = Promise.resolve();
-      promise = promise
+      const promise = Promise.resolve()
+        // 执行一次排序
         .then(() => {
-          let result;
           if (this.data.endIndex && this.data.startIndex === this.data.endIndex) {
-            result = false;
-          } else if (this.data.endIndex) {
-            result = this.methods.quickSortOnce(this.data.startIndex, this.data.endIndex);
-          } else {
-            result = this.methods.quickSortOnce();
+            return false;
           }
-          return result;
-        });
-      promise = promise
+          return this.methods.quickSortOnce(this.data.startIndex, this.data.endIndex);
+        })
+        // 递归
         .then(() => {
-          // 循环 条件
-          let loop;
-          if (this.data.endIndex && this.data.endIndex !== this.data.startIndex) {
-            loop = Promise.resolve(this.methods.quickSortLoop());
-          } else {
+          if (!this.data.endIndex || this.data.endIndex === this.data.startIndex) {
             const item = this.data.items[this.data.startIndex];
             Dom.of(item.template).removeClass('yellow');
+            return false;
           }
-          return loop;
+          return this.methods.quickSortLoop();
         });
       return promise;
     },
     quickSortRecursion(start = 0, end = this.data.items.length - 1) {
-      if (start < end) {
-        console.log('Recursion', start, end);
-      } else {
-        return false;
-      }
-      // 不应该并联 使其串联就是最终结果
+      if (start >= end) { return false; }
+      console.log(`起点：${start},终点${end}`);
       // 递归
-      let flag;
-      let promise = Promise.resolve();
-      promise = promise
+      let pivotIndex;
+      const promise = Promise.resolve()
+        // 执行一次循环
         .then(() => {
           this.data.startIndex = start;
           this.data.endIndex = end;
-          let result = this.methods.quickSortLoop()
-            .then(() => {
-              flag = this.data.startIndex;
-            });
-          return result;
+          const loop = Promise.resolve()
+            .then(() => this.methods.quickSortLoop())
+            .then(() => { pivotIndex = this.data.startIndex; });
+          return loop;
         })
+        // 递归
         .then(() => {
-          let result = Promise.resolve();
-          result = result
+          const recursion = Promise.resolve()
             .then(() => {
-              return this.methods.quickSortRecursion(start, flag - 1);
+              this.data.items[pivotIndex].dispatchEvent('send', { backColor: '#ccc' });
             })
-            .then(() => {
-              return this.methods.quickSortRecursion(flag + 1, end);
-            })
-          return result;
-        })
+            .then(() => this.methods.quickSortRecursion(start, pivotIndex - 1))
+            .then(() => this.methods.quickSortRecursion(pivotIndex + 1, end));
+          return recursion;
+        });
       return promise;
-    },
-    getArray() {
-      const array = this.data.items.map(item => item.data.value);
-      this.data.array = array;
-      return array;
     },
   },
   created() {
